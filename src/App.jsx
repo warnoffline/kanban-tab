46d2373615ee798c951ebd48f7a9f5fb5f74b8ab
Warnoffline <hametshin543@gmail.com>
2023-11-16 21:40:21 +0300

    fix phone

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,15 @@ import Form from './components/Form';
 import ColumnTask from './components/ColumnTask';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
+import { TouchBackend } from 'react-dnd-touch-backend';
 import './App.css'
 
 const App = function () {
+    console.log(window.TouchEvent)
+    const isTouchDevice = 'ontouchstart' in window || navigator.maxTouchPoints > 0 || navigator.msMaxTouchPoints > 0;
+    console.log(isTouchDevice)
     return (  
-        <DndProvider backend={HTML5Backend}>
+        <DndProvider backend={isTouchDevice? TouchBackend : HTML5Backend}>
             <div className='list'>
                 <Form/>
                 <div className='columns'>
--- src/components/ColumnTask.jsx
+++ src/components/ColumnTask.jsx
@@ -9,17 +9,15 @@ import { Card, CardBody, CardHeader, Heading, Divider } from '@chakra-ui/react';
 const ColumnTask = function ({title}) {
     const tasks = useSelector(state => state.tasks)
     const dispatch = useDispatch()
-    const [{ isOver }, drop] = useDrop({
-        accept: 'TASK',
-        drop: (item) => {
-            if (item.column !== title) {
-              dispatch(moveTask(item.id, title));
-            }
-        },
-        collect: monitor => ({
-          isOver: !!monitor.isOver(),
-        }),
-      });
+    const [, drop] = useDrop({
+      accept: 'TASK',
+      hover: (item) => {
+        if (item.column !== title) {
+          dispatch(moveTask(item.id, title));
+          item.column = title;
+        }
+      },
+    });
 
     return (  
         <Card className='column' ref={drop}>
