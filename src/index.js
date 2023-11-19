8ec773205927db580c03d3db4b3e27cabfbcb2ae
Warnoffline <hametshin543@gmail.com>
2023-11-20 00:42:34 +0300

    spinner

--- src/index.js
+++ src/index.js
@@ -3,15 +3,26 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { store } from './store/index'
 import { Provider } from 'react-redux';
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, Spinner } from '@chakra-ui/react'
 import { ColorModeScript } from '@chakra-ui/react'
 import theme from './services/theme';
 import './18n'
+import './App.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}> 
-    <Suspense fallback={<div>Loading...</div>}>
+    <Suspense fallback={
+      <div className='spinner'>
+        <Spinner
+        thickness='4px'
+        speed='0.65s'
+        emptyColor='gray.200'
+        color='blue.500'
+        size='xl'
+        />
+      </div>
+    }>
       <ChakraProvider>
         <ColorModeScript initialColorMode={theme.config.initialColorMode} />
         <App />
